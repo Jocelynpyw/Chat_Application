8b707e80ada3f8e767d689b307297053db427cda
jocelyn pyw <jocelynwotcheu2@gmail.com>
2023-01-10 12:15:01 +0100

    application icon added

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -1,9 +1,13 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const InputBox = () => {
   return (
     <View>
+      <AntDesign name="home" size={90} color="#900" />
+
+      {/* <FontAwesome5 name="plus" key={123} size={20} color="yellow" /> */}
       <Text>InputBox</Text>
     </View>
   );
